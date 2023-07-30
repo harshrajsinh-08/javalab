75a9ff65a235a74e7aeaf51e148c68d9b829ad91
Harsh <harshrajsinhz1684@gmail.com>
2023-07-30 21:39:35 +0530

    factorial changed

--- fact.java
+++ fact.java
@@ -1,22 +1,22 @@
 import java.util.Scanner;
 
 public class fact {
-    public static int factorial(int number) {
-        if (number == 0 || number == 1) {
+    public static int factorial(int num) {
+        if (num == 0 || num == 1) {
             return 1;
         }
     
         int result = 1;
-        for (int i = 2; i <= number; i++) {
+        for (int i = 2; i <= num; i++) {
             result *= i;
         }
         return result;
     }
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
-        System.out.print("Enter a non-negative integer: ");
-        int num = scanner.nextInt();
+        System.out.print("Enter a number ");
+        int num = sc.nextInt();
 
         if (num < 0) {
             System.out.println("Factorial is not defined for negative numbers.");
@@ -25,6 +25,6 @@ public class fact {
             System.out.println("Factorial of " + num + " is: " + result);
         }
 
-        scanner.close();
+        sc.close();
     }
 }
