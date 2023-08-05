abf60da2c690c7c0914014b7994bcaa89de90e3f
Harsh <harshrajsinhz1684@gmail.com>
2023-08-06 00:34:51 +0530

    lab 1

---
+++ First assessment/test.java
@@ -0,0 +1,9 @@
+public class test {
+    public static void main(String[] args) {
+        System.out.println("HELLO");
+        String name = "+";
+        if(name == "+"){
+            System.out.println("correct");
+        }
+    }
+}
