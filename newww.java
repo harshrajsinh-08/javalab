271293cdcd47ee898c67ac24178c782cc0e63b63
Harsh <harshrajsinhz1684@gmail.com>
2023-08-07 14:48:48 +0530

    lab 3

---
+++ newww.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class newww {
+    public static boolean isPIN(String pin) {
+        int length = pin.length();
+        
+        if (length != 4 && length != 6 && length != 8) {
+            return false;
+        }
+
+        for (int i = 0; i < length; i++) {
+            if (Character.isDigit(pin.charAt(i)) == false) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a PIN: ");
+        String pin = scanner.nextLine();
+
+        if (isPIN(pin)) {
+            System.out.println("Valid PIN");
+        } else {
+            System.out.println("Invalid PIN");
+        }
+
+        scanner.close();
+    }
+}
