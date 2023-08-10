83cc11b005a3c0c230c21b1bef69797bec96257f
Harsh <harshrajsinhz1684@gmail.com>
2023-08-10 15:29:18 +0530

    assignment 7 questions

--- AUG_9_wed/trrr.java
+++ AUG_9_wed/trrr.java
@@ -2,13 +2,12 @@ package AUG_9_wed;
 
 import java.util.Arrays;
 import java.util.Scanner;
-public class arr{
+public class trrr{
   public static int removeDuplicates(int arr[], int count){
     if (count==0 || count==1){
       return count;
     }
-    
-    //temp array kyuki kahi to store karna hai !
+    // creating a temporary array to hold non-duplicate elements
     int[] temp = new int[count];
     int j = 0;
     for (int i=0; i<count-1; i++){
@@ -17,8 +16,7 @@ public class arr{
       }
     }
     temp[j++] = arr[count-1];
-    
-    //temp ko vaapis original mai store karna:
+    // copying the temp array to the original array
     for (int i=0; i<j; i++){
       arr[i] = temp[i];
     }
@@ -33,16 +31,18 @@ public class arr{
         arr[i] = sc.nextInt();
     }
 
-    //Sort karna 
+    //Sorting the given unsorted array
     Arrays.sort(arr);
     int length = arr.length;
-    
-    //naye array ki size nikalna
+    //getting the new array size after removing duplicates
     length = removeDuplicates(arr, length);
+
+    //for new line
+    System.out.println("");
 
     //display
 	System.out.print(length+ " ");
     for (int i=0; i<length; i++)
       System.out.print(arr[i]+" ");
-  }	 	  	 	  	   	      	  	  	    	       	 	
+  }
 }
