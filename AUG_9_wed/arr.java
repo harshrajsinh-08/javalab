569ec50033e8ac087a4c13053bcbd9373e7e345c
Harsh <harshrajsinhz1684@gmail.com>
2023-08-10 15:29:18 +0530

    assignment 7 questions

--- AUG_9_wed/arr.java
+++ AUG_9_wed/arr.java
@@ -29,8 +29,9 @@ public class arr{
 
   public static void main (String[] args) {
     Scanner sc = new Scanner(System.in);
-    int arr[] = new int[7];
-    for(int i = 0;i<7;i++){
+    int n = sc.nextInt();
+    int arr[] = new int[n];
+    for(int i = 0;i<n;i++){
         arr[i] = sc.nextInt();
     }
 
