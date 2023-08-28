29ced789fd44e2d928ac9cc5a5baa8eef16b4c24
Harsh <harshrajsinhz1684@gmail.com>
2023-08-28 21:08:35 +0530

    deleted

--- assignment_hw/q1.java
+++ assignment_hw/q1.java
@@ -1,8 +1,12 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 
 =======
 package assignment_hw;
 >>>>>>> e8c83f3 (made the assignment folder)
+=======
+
+>>>>>>> ee7d9cb (deleted)
 import java.util.Scanner;
 public class q1 {
     public static void main(String[] args) {
