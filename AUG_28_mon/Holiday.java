5cb7658204d549f13f1f0531458de00015125304
Harsh <harshrajsinhz1684@gmail.com>
2023-08-28 21:07:59 +0530

    Aug_28

---
+++ AUG_28_mon/Holiday.java
@@ -0,0 +1,77 @@
+<<<<<<< HEAD
+=======
+package AUG_28_mon;
+import java.util.Scanner;
+
+public class Holiday {
+    private String name;
+    private int day;
+    private String month;
+
+    public Holiday(String name, int day, String month) {
+        this.name = name;
+        this.day = day;
+        this.month = month;
+    }
+
+    public boolean inSameMonth(Holiday otherHoliday) {
+        return this.month.equals(otherHoliday.month);
+    }
+
+    public static double avgDate(Holiday[] holidays) {
+        int totalDays = 0;
+        for (Holiday holiday : holidays) {
+            totalDays += holiday.day;
+        }
+        return (double) totalDays / holidays.length;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the number of holidays: ");
+        int numHolidays = scanner.nextInt();
+        scanner.nextLine();
+
+        Holiday[] holidays = new Holiday[numHolidays];
+
+        for (int i = 0; i < numHolidays; i++) {
+            System.out.print("Enter name of holiday " + (i + 1) + ": ");
+            String name = scanner.nextLine();
+
+            System.out.print("Enter day of month for holiday " + (i + 1) + ": ");
+            int day = scanner.nextInt();
+            scanner.nextLine();
+
+            System.out.print("Enter month for holiday " + (i + 1) + ": ");
+            String month = scanner.nextLine();
+
+            holidays[i] = new Holiday(name, day, month);
+        }
+
+        System.out.println("Holidays:");
+        for (Holiday holiday : holidays) {
+            System.out.println("Name: " + holiday.name);
+            System.out.println("Day: " + holiday.day);
+            System.out.println("Month: " + holiday.month);
+        }
+
+        System.out.println("Average day: " + avgDate(holidays));
+
+        System.out.print("Enter the index of the first holiday to compare: ");
+        int index1 = scanner.nextInt();
+        System.out.print("Enter the index of the second holiday to compare: ");
+        int index2 = scanner.nextInt();
+
+        if (index1 >= 0 && index1 < numHolidays && index2 >= 0 && index2 < numHolidays) {
+            if (holidays[index1].inSameMonth(holidays[index2])) {
+                System.out.println("The holidays are in the same month.");
+            } else {
+                System.out.println("The holidays are not in the same month.");
+            }
+        } else {
+            System.out.println("Invalid indices entered.");
+        }
+    }
+}
+>>>>>>> 82327fd (Aug_28)
