ed88358cc6cc64b5b0934dfbcfca5fc702b42831
Harsh <harshrajsinhz1684@gmail.com>
2023-08-28 21:08:35 +0530

    deleted

---
+++ assignment_hw/q1.java
@@ -0,0 +1,11 @@
+package assignment_hw;
+import java.util.Scanner;
+public class q1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String one = sc.nextLine();
+        one =one.toUpperCase();
+        System.out.println(one);
+    }
+}
+
