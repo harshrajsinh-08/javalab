b4a6f0cbf74accf826c6cc6e391edf3e5aa40650
Harsh <harshrajsinhz1684@gmail.com>
2023-08-30 15:08:09 +0530

    added bank

--- AUG_7_mon/duplicate.java
+++ AUG_7_mon/duplicate.java
@@ -31,6 +31,7 @@ public class duplicate {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int[] arr = new int[n];
+
         for(int i=0;i<n;i++){
             arr[i]=sc.nextInt();
         }
