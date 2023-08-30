1c659560d4fabc984c70f23d2ff38b279ed1d02f
Harsh <harshrajsinhz1684@gmail.com>
2023-08-30 15:08:09 +0530

    added bank

--- AUG_2_wed/attendence.java
+++ AUG_2_wed/attendence.java
@@ -4,18 +4,23 @@ public class attendence{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         // System.out.println("HELLO");
-        float total = sc.nextFloat();
-        float attended = sc.nextFloat();
-        float percent = (attended/total)*100;
-        // System.out.println(percent);
-        System.out.println("Attendance Percentage = "+(int)percent);
-        if(percent >=75){
-            System.out.println("Permitted to attend Exam");
-        }
-        else{
-            System.out.println("Not Permitted to attend Exam");
+        String total = sc.nextLine();
+        String total2 = sc.nextLine();
+        // System.out.printf("%s is this",total);
+        System.out.println(total + "is this ");
+        System.out.println(total2);
+        // String xyz = sc.next();
+        // float attended = sc.nextFloat();
+        // float percent = (attended/total)*100;
+        // // System.out.println(percent);
+        // System.out.println("Attendance Percentage = "+(int)percent);
+        // if(percent >=75){
+        //     System.out.println("Permitted to attend Exam");
+        // }
+        // else{
+        //     System.out.println("Not Permitted to attend Exam");
             
-        }
+        // }
         
         
     }
